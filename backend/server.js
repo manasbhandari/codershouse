dad7c5b616546ef029afe81795e677721e709122
MANAS BHANDARI <107706027+manasbhandari@users.noreply.github.com>
2024-06-08 18:10:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,23 @@ app.use(router);
 
 DbConnect();
 
-app.get("/", (req, res) => {
-  res.send("Hello from express js");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello from express js");
+// });
 
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 //socket connection
 const socketUserMap = {};
