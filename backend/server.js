4d756ed5d44b91a0069e11d141a232c8bd79c71b
MANAS BHANDARI <107706027+manasbhandari@users.noreply.github.com>
2024-06-17 17:29:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,6 @@ const io = require("socket.io")(server , {
 });
 
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-});
 
 
 const corsOption = {
